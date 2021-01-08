d56ee3fabd7dccc8d4b2935c2fde5d36f6e8ece4
Huy <nguyenhuuhuy203@gmail.com>
2021-01-08 09:09:21 +0700

    first commit - 8/1/21

---
+++ app.js
@@ -0,0 +1,5 @@
+const notificationElement = document.querySelector(".notification");
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
